b78c6b6d74a20fee685f58fed513b0af2ecf3dfa
AnthonyFinney <anthonyrockfinnie0@gmail.com>
2025-07-02 23:57:50 +0600

    3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import Beams from "./components/blocks/Backgrounds/Beams/Beams";
+
 function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
-        </>
+        <div>
+            <div className="w-full h-[100vh] relative">
+                <Beams
+                    beamWidth={2}
+                    beamHeight={15}
+                    beamNumber={12}
+                    lightColor="#ffffff"
+                    speed={2}
+                    noiseIntensity={1.75}
+                    scale={0.2}
+                    rotation={0}
+                />
+                <h1 className="text-3xl font-bold underline">Hello World</h1>
+            </div>
+        </div>
     );
 }
 
